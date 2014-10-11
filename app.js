33454e559cc145f45b1496cbd555116497ef2411
anglee <ang.lee@gmail.com>
2014-10-10 22:26:08 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+(function(angular, _) {
+  "use strict";
+
+  var module = angular.module("mainApp", ["my.components"]);
+  module.controller('mainCtrl', function($scope) {
+    $scope.foo = "AngularJS";
+  });
+
+})(angular, _);
