d95de9dc411452b4825666ed8d512a2c3e128f8e
anglee <ang.lee@gmail.com>
2015-03-03 02:38:46 -0500

    Add tween for radial arc and text

--- my_components/d3/radial.js
+++ my_components/d3/radial.js
@@ -30,6 +30,22 @@
         return d * Math.PI / 180;
       });
 
+  var arcTween = function(datum, index, attr) {
+    var interpolateFunc = d3.interpolate(datum.transitionStartDegree, datum.transitionEndDegree);
+    return function(t) { // t will grow from 0 to 1 during the transition
+      var endAngle = interpolateFunc(t);
+      datum.endAngle = endAngle;
+      return arcFunction(endAngle);
+    };
+  };
+  var textTween = function(datum) {
+    var interpolateFunc = d3.interpolate(datum.transitionStartDegree, datum.transitionEndDegree);
+    return function(t) { // t will grow from 0 to 1 during the transition
+      var endAngle = interpolateFunc(t);
+      this.textContent = endAngle.toFixed(0);
+    };
+  };
+
   var enter = svg.selectAll('path').data([data]).enter()
 
   var path = enter.append('path')
@@ -41,14 +57,7 @@
       })
       .transition()
       .duration(2000)
-      .attrTween("d", function(datum, index, attr) {
-        var interpolateFunc = d3.interpolate(datum.transitionStartDegree, datum.transitionEndDegree);
-        return function(t) { // t will grow from 0 to 1 during the transition
-          var endAngle = interpolateFunc(t);
-          datum.endAngle = endAngle;
-          return arcFunction(endAngle);
-        };
-      });
+      .attrTween("d",arcTween);
 
   var text = enter.append('text')
       .attr("class", "label")
@@ -57,12 +66,21 @@
       .style("font-size", _fontSize + "px")
       .transition()
       .duration(2000)
-      .tween("text", function(datum) {
-        var interpolateFunc = d3.interpolate(datum.transitionStartDegree, datum.transitionEndDegree);
-        return function(t) { // t will grow from 0 to 1 during the transition
-          var endAngle = interpolateFunc(t);
-          this.textContent = endAngle.toFixed(0);
-        };
-      });
+      .tween("text", textTween);
+
+  svg.on("click", function() {
+    console.log("clicked", data);
+    data.transitionStartDegree = data.endAngle;
+    data.transitionEndDegree = _.random(0, 360);
+    svg.selectAll('path')
+        .transition()
+        .duration(2000)
+        .attrTween("d",arcTween);
+    svg.selectAll('text')
+        .transition()
+        .duration(2000)
+        .tween("text", textTween);
+  })
+
 
 })();
