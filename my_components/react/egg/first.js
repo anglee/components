5615dbf0f04cf7d737d612b9916d815ebd0ced86
anglee <ang.lee@gmail.com>
2015-03-04 07:04:16 -0500

    Add a isRequired prop but there is no warning in the console, wonder why

--- my_components/react/egg/first.js
+++ my_components/react/egg/first.js
@@ -1,9 +1,10 @@
 var App = React.createClass({
   propType: {
-    txt: React.PropTypes.string
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
   },
   render: function() {
     return (<h1>Hello, { this.props.txt }</h1>)
   }
 });
-React.renderComponent(<App txt="World" />, document.body);
+React.render(<App txt="World" />, document.body);
