e477c4d84052923d2b4f73d95d411a68ba3506f3
anglee <ang.lee@gmail.com>
2015-03-23 10:19:36 -0400

    Add example of using mocha with Promise

--- experiments/karma/aysnc.mspec.js
+++ experiments/karma/aysnc.mspec.js
@@ -2,6 +2,7 @@ var expect = chai.expect;
 
 describe("async", function() {
   describe("foo", function() {
+    // use done()
     it("should say Hello asynchronously", function(done) {
       async.foo().then(function(what) {
         expect(what).to.equal("Hello");
@@ -10,14 +11,27 @@ describe("async", function() {
         done(err);
       });
     });
+    // use Promise
+    it("should say Hello as promised", function() {
+      return async.foo().then(function(what) {
+        expect(what).to.equal("Hello");
+      });
+    });
   });
   describe("oops", function() {
+    // use done()
     it("should throw Sorry", function(done) {
       async.oops().then(function(what) {
         done(new Error("Didn't throw"));
       }, function(err) {
         expect(err.message).to.equal("Sorry");
         done();
+      });
+    });
+    // use Promise
+    it("should throw Sorry as promised", function() {
+      return async.oops().catch(function(err) {
+        expect(err.message).to.equal("Sorry");
       });
     });
   });
