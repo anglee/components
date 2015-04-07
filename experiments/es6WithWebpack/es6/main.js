3d2a1b7342def0ffd5a4203783e525b8c3830002
anglee <ang.lee@gmail.com>
2015-04-06 21:49:47 -0400

    Make webpack babel work with es6 generator example

--- experiments/es6WithWebpack/es6/main.js
+++ experiments/es6WithWebpack/es6/main.js
@@ -1,3 +1,16 @@
 import Point from './Point.js';
 var body = document.querySelector('body');
 body.textContent = 'Good point: ' + new Point(1, 23);
+
+
+function* genFunc() {
+  console.log('First');
+  yield; // (A)
+  console.log('Second'); // (B)
+}
+
+let genObj = genFunc();
+
+console.log(genObj.next());
+
+console.log(genObj.next());
