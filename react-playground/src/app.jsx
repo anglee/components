7e62f7c289655949125dda2f2fcad4bb12be2870
anglee <ang.lee@gmail.com>
2015-04-16 05:50:25 -0400

    Reflux actions

--- react-playground/src/app.jsx
+++ react-playground/src/app.jsx
@@ -1,7 +1,20 @@
 var React = require("react");
 var Reflux = require("reflux");
 
+var actionsSet1 = Reflux.createActions(["updateAaa", "updateBbb"]);
+var actionsSet2 = Reflux.createActions(["updateCcc", "ddd"]);
+
 var store = Reflux.createStore({
+    listenables: [actionsSet1, actionsSet2],
+    onUpdateAaa() {
+      console.log("I am aaa");
+    },
+    onUpdateBbb() {
+      console.log("I am bbb");
+    },
+    onUpdateCcc(foo) {
+      console.log("I am ccc " + foo);
+    },
     data: {
         items: [],
         tick: 1
@@ -34,7 +47,10 @@ var App = React.createClass({
             <h1>Hello change </h1>
             { this.state.items.map(it => <div>{it.text}</div>) }
             <div>{ this.state.tick }</div>
-            <span>1 + 1 = { foo } (or { bar })</span>
+            <div>1 + 1 = { foo } (or { bar })</div>
+            <button onClick={actionsSet1.updateAaa}>aaa</button>
+            <button onClick={actionsSet1.updateBbb}>bbb</button>
+            <button onClick={actionsSet2.updateCcc.bind(this, new Date())}>ccc</button>
         </div>);
     }
 });
