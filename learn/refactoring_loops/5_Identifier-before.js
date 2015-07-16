00a1d8bc6878e9d54dbb9399965592805834fbf3
anglee <ang.lee@gmail.com>
2015-07-15 23:36:50 -0400

    Identifier

---
+++ learn/refactoring_loops/5_Identifier-before.js
@@ -0,0 +1,57 @@
+
+var ids = [{
+  scheme: "x"
+}, {
+  scheme: "y"
+}, {
+  scheme: "z"
+}];
+
+var check_valid_ids = function(required_schemes) {
+  var errors = [];
+
+  var used = [];
+  var dups = [];
+  var not_found_required = required_schemes.slice(0);
+
+  for (var id of ids) {
+    if (id.void) {
+      continue;
+    }
+    if (used.indexOf(id.scheme) !== -1) {
+      dups.push(id.scheme);
+    } else {
+      for (var req of required_schemes) {
+        if (id.scheme === req) {
+          not_found_required.splice(not_found_required.indexOf(req), 1);
+        }
+      }
+    }
+
+    used.push(id.scheme);
+  }
+
+  if (dups.length > 0) {
+    errors.push("duplicated schemes: " + dups.join(", "));
+  }
+
+  if (not_found_required.length > 0) {
+    var missing_names = "";
+    for (req of not_found_required) {
+      missing_names += (missing_names.size > 0) ? ", " + req : req
+    }
+    errors.push("missing schemes: " + missing_names);
+  }
+
+  return errors.length ? errors : "valid";
+};
+
+console.log(check_valid_ids(["x", "y"]));
+console.log(check_valid_ids(["a", "y"]));
+
+ids.push({
+  scheme: "x"
+});
+
+console.log(check_valid_ids(["x", "y"]));
+
